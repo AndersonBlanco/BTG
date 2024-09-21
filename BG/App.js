2b1ffa05b113fb2d3367e6b20f6894d094a895df
AndersonBlanco <andersonblanco913@gmail.com>
2024-09-21 14:50:01 -0400

    base

---
+++ BG/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Text>Hello Universe</Text>
+    <Button title="Test" onPress={()=>alert("Hello Universe")}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
